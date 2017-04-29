58fd34d2841bc552509c0c3800a47bd7da470fa8
Hugues Lerebours <hugues@lereboursp.net>
2017-04-29 22:35:24 +0200

    Update setup.py: unsupported versions
    notably an issue with pylint 1.7.1

--- setup.py
+++ setup.py
@@ -17,12 +17,10 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: Implementation :: CPython',
-    'Programming Language :: Python :: Implementation :: PyPy',
     'Topic :: Software Development :: Libraries :: Python Modules'
 ]
 
@@ -39,7 +37,7 @@ if __name__ == '__main__':
         url="https://github.com/hlerebours/lambda-calculus",
         license="MIT License",
         packages=['lambdax', 'lambdax.test'],
-        setup_requires=['pytest-runner>=2.11', 'pylama_pylint>=3.0'],
-        tests_require=['pytest>=3.0', 'pytest-cov>=2.4', 'pylint>=1.6', 'pylama>=7.3'],
+        setup_requires=['pytest-runner>=2.11', 'pylama_pylint>=3.0', 'pylint==1.7.0'],
+        tests_require=['pytest>=3.0', 'pytest-cov>=2.4', 'pylama>=7.3'],
         platforms=['any']
     )
